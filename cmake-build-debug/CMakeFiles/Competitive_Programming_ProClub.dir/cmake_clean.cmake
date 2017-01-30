4753a8af6ed52d665c6179810ae23b68dd0c56d2
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-01-30 11:06:45 +0700

    init

---
+++ cmake-build-debug/CMakeFiles/Competitive_Programming_ProClub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Competitive_Programming_ProClub.dir/main.cpp.o"
+  "Competitive_Programming_ProClub.pdb"
+  "Competitive_Programming_ProClub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Competitive_Programming_ProClub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Competitive_Programming_ProClub.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Competitive_Programming_ProClub.
+# This may be replaced when dependencies are built.
